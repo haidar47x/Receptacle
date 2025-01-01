a4e95126f97e046cb858773a133a3d57b3d13ca0
Haidar Ali <haidar47x@gmail.com>
2025-01-01 23:56:55 +0500

    Added base Feed and FeedItems classes

---
+++ Models/Feed/Feed.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receptacle.Models.Feed {
+    internal class Feed {
+        private IList<FeedItem> _feedItems;
+        
+        public Feed() { }
+
+        public Feed(IList<FeedItem> feedItems) {
+            _feedItems = feedItems;
+        }
+
+        public IList<FeedItem> feedItems { get { return _feedItems; } }
+
+    }
+}
---
+++ Models/Feed/FeedItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Receptacle.Models.Feed {
+    internal class FeedItem {
+        private string id { get; set; }
+        private string title { get; set; } 
+        private string url { get; set; }
+        private string author { get; set; }
+        private string description { get; set; }
+
+    }
+}
