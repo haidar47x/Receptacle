67989f4f58b9628b05d0e4b9454dc5078bac9cb6
Haidar Ali <haidar47x@gmail.com>
2025-01-04 02:40:27 +0500

    Restructured the repository

--- src/Receptacle/App.xaml.cs
+++ src/Receptacle/App.xaml.cs
@@ -16,22 +16,31 @@ using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
 namespace Receptacle {
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
     public partial class App : Application {
-
-        private Window? m_window;
-
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
         public App() {
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Invoked when the application is launched.
+        /// </summary>
+        /// <param name="args">Details about the launch request and process.</param>
         protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args) {
-            m_window = new Views.MainWindow();
-            if (m_window == null) {
-                Console.WriteLine("Main window is null.");
-            } else {
-                m_window.Activate();
-            }
+            m_window = new MainWindow();
+            m_window.Activate();
         }
+
+        private Window? m_window;
     }
 }
